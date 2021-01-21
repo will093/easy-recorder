fe5904e91f66543feaa8a3ddb428ae51ab1485ba
Will Taylor <willtaylor.developer@gmail.com>
2021-01-21 18:51:50 +0000

    Improve look and feel of footer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { TimerPipe } from './pipes/timer/timer.pipe';
 import { HeaderComponent } from './components/header/header.component';
 import { IonicModule } from '@ionic/angular';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     TimerPipe,
     HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     CommonModule,
@@ -16,6 +18,7 @@ import { IonicModule } from '@ionic/angular';
   exports: [
     TimerPipe,
     HeaderComponent,
+    FooterComponent,
   ]
 })
 export class SharedModule { }
