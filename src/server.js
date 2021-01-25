52c3eb56411e61100f3d413cab3b2b6e43c1534b
Will Taylor <willtaylor.developer@gmail.com>
2021-01-25 12:05:58 +0000

    routing fix for heroku

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 /**
  * Used to serve the app when deployed on Heroku
  */
-
-var express = require('express'), app = express();
+let path = require('path');
+let express = require('express'), app = express();
+let indexHtmlPath = path.resolve('./www/index.html');
 
 app.use(express.static('www'));
 app.set('port', process.env.PORT || 5000);
+app.get('/*', (req, res) => res.sendFile(indexHtmlPath));
+
 app.listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
 });
